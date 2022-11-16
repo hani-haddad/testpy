23c824f307260fc347c8b47a3b09eb0a02b746ad
Hani Haddad <hani@capella.io>
2022-11-16 15:40:12 +0200

    Initial

---
+++ testbs4.py
@@ -0,0 +1,16 @@
+# import requests
+
+from bs4 import BeautifulSoup
+# req = requests.get('https://www.floridabar.org/directories/find-mbr/profile/?num=171560')
+
+with open(r'htmls/testhtml.html', "r") as f:
+    page = f.read()
+soup = BeautifulSoup(page , 'html.parser')
+
+
+
+
+def name_extraxt(soup):
+    print(soup.findAll('title'))
+    titleTag = soup.findAll('title')[0]
+    return titleTag.text.split('–')[1].strip()
