c36bad07081be13db171477361060f227b68d5df
shathaq <shashaqaruti.k99@gmail.com>
2022-12-01 14:59:45 +0300

    added modifications

--- stackOverflowSraper/exporter.py
+++ stackOverflowSraper/exporter.py
@@ -1,30 +1,15 @@
 import pandas as pd
-import csv
-import os.path
+
 
 class exporter():
-    data_frame = pd.DataFrame()
+    data=[]
+    data_frame = pd.DataFrame(data)
 
 
-    def csv_export(self, Question):
-        '''Write item to CSV file'''
+    def csv_export(self):
+        self.data_frame.to_csv('Questions_Details.csv')
 
-        # Check if "Questions Details.csv" file exists
-        questions_exists = os.path.isfile('Questions_Details.csv')
 
-        # Append data to CSV file
-        with open('Questions_Details.csv', 'a') as csv_file:
-            # Init dictionary writer
-            writer = csv.DictWriter(csv_file, fieldnames=Question.keys())
-
-            # Write entry to CSV file
-            writer.writerow(Question)
-
-    
     def json_export(self):
         self.data_frame.to_json('Questions_Details.json', orient='records', lines=True)
-
-
-
-    
 
