8dc13e34f49822804c9b097247b5eefb9215cf43
shathaq <shashaqaruti.k99@gmail.com>
2022-12-01 12:38:55 +0300

    fix UnicodeEncodeError: 'charmap' codec error by passing the encoding='utf-8' parameter

--- stackOverflowSraper/download_htmls.py
+++ stackOverflowSraper/download_htmls.py
@@ -19,23 +19,14 @@ class download_htmls:
 
 #call it write html file to location and you need another function to setup location
     def write_html(self,File,html):
-      with open(File , 'w+') as File:
+      with open(File , 'w+',encoding='utf-8') as File:
             File.write(html)
 
 # you can call this get_soup and send the file path to it no need to
 #  read by line and send the file path to the soup you can move the file location 
     def get_soup(self,File):
-      with open(File , 'r') as File:
+      with open(File , 'r',encoding='utf-8') as File:
         html= File.read()
         soup = BeautifulSoup(html,'html.parser')  
         return soup
-
-obj= download_htmls
-y=obj.Request("kkk","https://stackoverflow.com/questions/tagged/python?tab=Active")
-file_path=obj.open_file("kkk","p.html")
-
-obj.write_html("kkk",file_path,y.text)
-soup = obj.get_soup("kkk",file_path)
-print(soup)
-
 
