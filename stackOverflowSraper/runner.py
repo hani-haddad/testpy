585d67c9dfeab13fabc6155eaf2be2d31deff4c1
shathaq <shashaqaruti.k99@gmail.com>
2022-12-01 14:23:07 +0300

    create another file called runner to start scrapping process

---
+++ stackOverflowSraper/runner.py
@@ -0,0 +1,10 @@
+import scraper
+
+class Main(scraper):
+
+    if __name__ == '__main__':
+        # Init scraper instance
+        scraper = scraper()
+
+        # Start scraper
+        scraper.run("python")
