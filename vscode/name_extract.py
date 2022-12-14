c6f5c54d472dc60f0cf3604cff528a06bf40e477
shathaq <shashaqaruti.k99@gmail.com>
2022-12-14 17:39:33 +0300

    no message

--- vscode/name_extract.py
+++ vscode/name_extract.py
@@ -2,8 +2,8 @@ import re
 class full_name():
     suffixes=[' I ',' II ',' III ',' IV ',' V ',' Senior ',' Junior ','Jr',' Sr ',' PhD ',' APR ',' RPh ',' PE ',' MD ',' MA ',' DMD ',' CME ']
     honerfixse=["Mr.","master","mister","Mrs.","Dr.","Rev.","Fr."]
-    search_header_tags=["h1","h2"]
-    search_tags=[]
+    #search_header_tags=["h1","h2"]
+    search_tags=["h1","h2"]
     search_class_names=[]
     re_name_mid="full_name"
     suffixes_string='|'.join(suffixes)
@@ -22,24 +22,26 @@ class full_name():
     
      elif name :=self.find_specialCase_tag(soup):
         return name
+        
      soup.find_all(self.search_header_tags,string=re.complie(self.re_compund_name,re.I))
 
     def clean_up_name(name):
         return
 
     def find_title_tag(self,soup):
-        title=soup.find("title",string=re.complie(self.re_compund_name,re.I)).text
+        name_regex = re.compile(self.re_compund_name,re.I)
+        title=soup.find("title",string=name_regex).text
         title_list=re.split(',|_|-|!', title)[0]
-        p = re.compile(self.re_compund_name)
-        res= [p.sub('', x).strip() for x in title_list]
+        res= [name_regex.sub('', x).strip() for x in title_list]
         return res if(len(res)>0) else  None 
 
-    def find_header_tag(soup):
-        return
+    def find_header_tag(self,soup):
+          names=soup.find_all(self.search_tags,string=re.compile(self.re_compund_name,re.I))
+          [self.search_class_names.append(tag["class"]) if tag["class"] not in self.search_tags else  None for tag in names]
+          return [name.text for name in names] if(len(names)>0)  else  None
 
     def find_by_class(self,soup,tag=""):
-        for class_name in self.search_class_names:
-          names=soup.find_all(tag,class_name,string=re.compile(self.re_compund_name,re.I))
+          names=soup.find_all(tag,self.search_class_names,string=re.compile(self.re_compund_name,re.I))
           [self.search_tags.append(tag.name) if tag.name not in self.search_tags else  None for tag in names]
           return [name.text for name in names] if(len(names)>0)  else  None
         
