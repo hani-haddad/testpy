4d09cd40d0641314c99dabd5aea87ed49f140654
shathaq <shashaqaruti.k99@gmail.com>
2022-12-14 13:53:45 +0300

    implemnt find_title_tag method

--- vscode/name_extract.py
+++ vscode/name_extract.py
@@ -6,21 +6,24 @@ class full_name():
     re_name_mid="full_name"
     suffixes_string='|'.join(suffixes)
     re_compund_name= rf'({suffixes_string}){re_name_mid}' 
-    def name_extrator(soup):
+
+
+    def name_extrator(self,soup):
      
-     soup.find_all(search_header_tags,string=re.complie(re_compund_name,re.I))
+     soup.find_all(self.search_header_tags,string=re.complie(self.re_compund_name,re.I))
 
     def clean_up_name(name):
         return
 
-    def find_title_tag(soup):
-        title=soup.find_all("title",string=re.complie(re_compund_name,re.I)).text
-        res = re.split(', |_|-|!', data)
-        return
+    def find_title_tag(self,soup):
+        title=soup.find_all("title",string=re.complie(self.re_compund_name,re.I)).text
+        title_list=re.split(',|_|-|!', title)[0]
+        p = re.compile(self.re_compund_name)
+        res= [p.sub('', x).strip() for x in title_list]
+        return res if(len(res)>0) else  None 
 
     def find_header_tag(soup):
         return
 
     def find_specialCase_tag(soup):
         return
-
