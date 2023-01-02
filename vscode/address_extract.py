62a0cbc9ed2bc3cb1c7c34bd7fc064d1fde86965
shathaq <shashaqaruti.k99@gmail.com>
2023-01-02 12:29:16 +0300

    no message

--- vscode/address_extract.py
+++ vscode/address_extract.py
@@ -1,21 +1,27 @@
 import re
 import itertools 
+import os
+from serpapi import GoogleSearch
 
 class Address:
     re_postel_code ='\s\d{5}'
-    re_region = '[A-Z]{2}' #,\s[A-Z]{2}
-    re_locality='[A-Z]\w+,'
-    re_adr_name= rf'^({re_locality}\s?{re_region}({re_postel_code}\s?' 
+    re_region = ',?\s([A-Z][A-Z]|[A-Z]\w+(\s[A-Z]\w+)?)'
+    re_locality='[A-Z]\w+(\s[A-Z]\w+)?,'
+    re_streetAddress='(^\d{3,4})?[a-z]*\s?([A-Z]\w+)?(\s[A-Z]\w+)?\s?#?(\d{3,4})?'
     ['street','street_1','state_1','zip_1']
+    re_adr_name= rf'({re_streetAddress})?,?\s({re_locality})?\s?({re_region})?,?\s({re_postel_code})?\s?' 
     search_tags=[]
     search_phrases_address=["addr","home"]
 
     def address_extrator(self,soup):
-     if name :=self.find_by_class_name(soup):
-        return name
+     if address :=self.find_by_class_name(soup):
+        return address
     
-     elif name :=self.find_in_script_tag(soup):
-        return name
+     elif address :=self.find_in_script_tag(soup):
+        return address
+      
+     elif address :=self.google_map_search(soup):
+        return address
 
     def clean_up_address(self,address):
       return address
@@ -68,3 +74,21 @@ class Address:
                       print(x1,"=>",y1)
         elif re.compile("(var\s)(street|state|zip|postal_code)", re.IGNORECASE).search(script.text):
             print(script)
+
+    def google_map_search(self):
+        params = {
+            "engine": "google_maps",
+            "q": "S J Harris Law",
+            "type": "search",
+          
+            "api_key": '1d97aabdc07c5ca86da7698cb7f5b750ed1c0ffa7dae05a044c5f8f350e203ab'
+        }
+        client = GoogleSearch(params)
+        data = client.get_dict()
+
+        print(data)
+
+        for x, y in data.items():
+          if re.compile("address", re.IGNORECASE).search(str(y)):
+            for x1, y1 in data[x].items():
+              print(x1,": ",y1)
